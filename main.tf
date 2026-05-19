66a82eca809ac52490511c9136af988182fe1d55
MilesN29 <mdn29@pitt.edu>
2026-05-19 13:27:29 -0400

    Remove cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  # cloud {
+  #   organization = "policy-as-code-training"
+  #   workspaces {
+  #     name = "tf-vault-qa-mn"
+  #   }
+  # }
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -7,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-1"
+  region = "us-west-1"
 }
 
 data "aws_availability_zones" "available" {
@@ -16,7 +22,7 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.64.0"
+  version = "~> 5.0"
 
   cidr = "10.0.0.0/16"
 
@@ -109,8 +115,8 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count = var.instance_count
+  instance_type  = var.instance_type
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
